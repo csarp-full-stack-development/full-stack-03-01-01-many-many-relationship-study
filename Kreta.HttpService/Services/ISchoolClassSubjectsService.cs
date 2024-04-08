aa4aca0504ae5651cdd733c1d033bdcb8259cdb6
Csaba Gyuris <gyuris.csaba@vasvari.org>
2024-04-08 14:12:44 +0200

    HttpService rétegben a kapcsolat tábla service (SchoolClassSubjectService)

---
+++ Kreta.HttpService/Services/ISchoolClassSubjectsService.cs
@@ -0,0 +1,9 @@
+﻿using Kreta.Shared.Models.SwitchTable;
+
+namespace Kreta.HttpService.Services
+{
+    public interface ISchoolClassSubjectsService
+    {
+        public Task<List<SchoolClassSubjects>> SelectAllIncludedAsync();
+    }
+}
---
+++ Kreta.HttpService/Services/SchoolClassSubjectsService.cs
@@ -0,0 +1,45 @@
+﻿using Kreta.Shared.Assamblers;
+using Kreta.Shared.Dtos;
+using Kreta.Shared.Models.SwitchTable;
+using Kreta.Shared.Responses;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Net.Http.Json;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kreta.HttpService.Services
+{
+    public class SchoolClassSubjectsService : BaseService<SchoolClassSubjects, SchoolClassSubjectsDto>, ISchoolClassSubjectsService
+    {
+        public SchoolClassSubjectsService(IHttpClientFactory? httpClientFactory,SchoolClassSubjectsAssambler assambler) : base(httpClientFactory, assambler)
+        {
+        }
+
+        public async Task<List<SchoolClassSubjects>> SelectAllIncludedAsync()
+        {
+            if (_httpClient is not null)
+            {
+                try
+                {
+
+                    List<SchoolClassSubjectsDto>? resultDto = await _httpClient.GetFromJsonAsync<List<SchoolClassSubjectsDto>>($"api/SchoolClassSubjects/included");
+                    if (resultDto is not null)
+                    {
+                        List<SchoolClassSubjects> result = resultDto.Select(entity => _assambler.ToModel(entity)).ToList();
+                        return result;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Debug.WriteLine(ex.Message);
+                }
+            }
+            return new List<SchoolClassSubjects>();
+        }
+    }
+}
